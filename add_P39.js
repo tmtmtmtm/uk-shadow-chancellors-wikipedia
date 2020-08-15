ef97e581a9a8ff006ef79366db4d884397ea362b
Tony Bowden <tony@tmtm.com>
2020-08-15 09:36:55 +0100

    Config via add_P39

--- add_P39.js
+++ add_P39.js
@@ -16,11 +16,11 @@ module.exports = (id, startdate, enddate, replaces, replacedby, ordinal, cabinet
     id,
     claims: {
       P39: {
-        value: 'Q1769526',
+        value: 'Q11013932',
         qualifiers: qualifiers,
         references: {
           P143: 'Q328', // English Wikipedia
-          P4656: 'https://en.wikipedia.org/wiki/Prime_Minister_of_Moldova'
+          P4656: 'https://en.wikipedia.org/wiki/Shadow_Chancellor_of_the_Exchequer'
         },
       }
     }
